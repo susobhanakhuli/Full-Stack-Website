dee194d55abc5d8a049e1138e9b028cfb83ef4a9
Susobhan Akhuli <susobhanakhuli8537@gmail.com>
2023-08-14 15:33:58 +0530

    adding border style in getmyprofile page

--- src/app/login/getmyprofile/page.js
+++ src/app/login/getmyprofile/page.js
@@ -60,46 +60,46 @@ export default function GetMyProfile() {
   };
 
   return (
-    <main className="bg-black text-white">
-      <div className="flex flex-col justify-center items-center text-center pb-16">
-        {/* Get My Profile API */}
+    <main className="bg-slate-300">
+      <div className="flex flex-col justify-center items-center text-center py-16">
+        <div className="p-8 border-4 border-yellow bg-black text-white shadow-2xl rounded-3xl">
+          {/* Get My Profile API */}
 
-        <h1 className="text-yellow">Get Your Profile</h1>
-        {/* <button onClick={getSavedToke}>Saved Token Details</button> */}
+          <h1 className="text-yellow">Get Your Profile</h1>
+          {/* <button onClick={getSavedToke}>Saved Token Details</button> */}
 
-        {/* Get My Profile */}
-        <button className="custom-button-2 my-5" onClick={getUserData}>{show ? "Hide Profile Details" : "Show Profile Details"}</button>
-        {userData && show && (
-          <div className="flex flex-col justify-center items-center text-center">
-            <h2>User Data</h2>
-            <Image
-              src={userData.profilePic}
-              alt="Profile Image"
-              className="profilePic"
-              width={200}
-              height={200}
-            >
-            </Image>
-            <p>Name: {userData.name}</p>
-            <p>Email: {userData.email}</p>
-            <p>Gender: {userData.gender}</p>
-            <p>Age: {userData.age}</p>
-            <p>Height (in cm): {userData.height}</p>
-            <p>Weight (in KG): {userData.weight}</p>
-            <p>Branch: {userData.branch}</p>
-            <div className="flex my-5 gap-4">
-              {/* Upload Profile Pic */}
-              <button className="custom-button-1 w-[11rem]" onClick={() => navigate("/login/uploadprofilepic")}>
-                Upload Profile Pic
-              </button>
+          {/* Get My Profile */}
+          <button className="custom-button-2 my-5" onClick={getUserData}>{show ? "Hide Profile Details" : "Show Profile Details"}</button>
+          {userData && show && (
+            <div className="flex flex-col justify-center items-center text-center">
+              <h2>User Data</h2>
+              <Image
+                src={userData.profilePic}
+                alt="Profile Image"
+                className="profilePic"
+                width={200}
+                height={200}
+              >
+              </Image>
+              <p>Name: {userData.name}</p>
+              <p>Email: {userData.email}</p>
+              <p>Gender: {userData.gender}</p>
+              <p>Age: {userData.age}</p>
+              <p>Height (in cm): {userData.height}</p>
+              <p>Weight (in KG): {userData.weight}</p>
+              <p>Branch: {userData.branch}</p>
+              <div className="flex my-5 gap-4">
+                {/* Upload Profile Pic */}
+                <button className="custom-button-1 w-[11rem]" onClick={() => navigate("/login/uploadprofilepic")}>
+                  Upload Profile Pic
+                </button>
 
-              {/* Log Out */}
-              <button className="custom-button-1" onClick={logOut}>Log Out</button>
+                {/* Log Out */}
+                <button className="custom-button-1" onClick={logOut}>Log Out</button>
+              </div>
             </div>
-          </div>
-        )}
-
-
+          )}
+        </div>
       </div>
     </main>
   );
